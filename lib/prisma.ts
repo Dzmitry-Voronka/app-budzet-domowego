26bbdbcea0cfe7f3e7d5a451dfd9e706756c2820
Dzmitry <voronindmitry07@gmail.com>
2026-04-28 22:47:45 +0200

    Use pg adapter for direct postgres:// connection

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
 function createPrismaClient() {
-  return new PrismaClient({
-    accelerateUrl: process.env.DATABASE_URL ?? "prisma+postgres://accelerate.prisma-data.net/?api_key=not-configured",
-  });
+  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
+  return new PrismaClient({ adapter });
 }
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
